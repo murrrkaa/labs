169f7be9c0f5946ca1e3a6a02b086128873da080
Maria Titova <159988109+murrrkaa@users.noreply.github.com>
2026-02-21 20:14:18 +0300

    Add files via upload

---
+++ МЛИТА/Lab13.4/13.4/13.4.cpp
@@ -0,0 +1,143 @@
+﻿//13.4.Циклический сдвиг(5)
+//Последовательность круглых скобок называют правильной, если можно добавить в нее числа и знаки 
+//арифметических операций так, что полученное алгебраическое выражение станет корректным.
+//Например, скобочная последовательность(())() является правильной, а(())) и())(() такими не являются.
+//    Задана скобочная последовательность из N символов(1 ≤ N ≤ 6×105).
+//    Найти минимальный номер позиции, начиная с которой последовательность, 
+//    образованная циклическим сдвигом, окажется правильной скобочной последовательностью.
+//    Ввод из файла INPUT.TXT.В первой строке задано значение N.Во второй строке 
+//    находится скобочная последовательность длины N.
+//    Вывод в файл OUTPUT.TXT.Вывести номер позиции.Если правильную скобочную 
+//    последовательность получить невозможно, вывести No.
+//    Примеры
+//    Ввод 1       Ввод 2       Ввод 3
+//    8            6            6
+//    ((())())     ()))((       ((())(  
+//        Вывод 1      Вывод 2      Вывод 3
+//        1            5            No
+// Бабина Мария ПС 21
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+const std::string INPUT_FILE_NAME = "input7.txt";
+const std::string OUTPUT_FILE_NAME = "output.txt";
+
+int findMinShiftPosition(int n, const std::string& s) 
+{
+    int openCount = 0, closeCount = 0;
+    for (char c : s) 
+    {
+        if (c == '(')
+        {
+            openCount++;
+        }
+        else
+        {
+            closeCount++;
+        }
+    }
+
+    if (openCount != closeCount) 
+    {
+        return -1;
+    }
+
+    int balance = 0;
+    int minBalance = 0;
+    int minPos = 0;
+
+    for (int i = 0; i < n; ++i) 
+    {
+        if (s[i] == '(') 
+        {
+            balance++;
+        }
+        else
+        {
+            balance--;
+        }
+
+        if (balance < minBalance) 
+        {
+            minBalance = balance;
+            minPos = i + 1;
+        }
+    }
+
+    balance = 0;
+    for (int i = 0; i < n; ++i) 
+    {
+        int pos = (minPos + i) % n;
+        if (s[pos] == '(') 
+        {
+            balance++;
+        }
+        else 
+        {
+            balance--;
+        }
+
+        if (balance < 0) 
+        {
+            return -1;
+        }
+    }
+
+    return (balance == 0) ? (minPos + 1) : -1;
+}
+
+bool readInput(int& n, std::string& s) 
+{
+    std::ifstream inputFile(INPUT_FILE_NAME);
+    if (!inputFile.is_open()) 
+    {
+        std::cout << "Failed to open input file!\n";
+        return false;
+    }
+    inputFile >> n >> s;
+    inputFile.close();
+    return true;
+}
+
+bool writeOutput(int result) 
+{
+    std::ofstream outputFile(OUTPUT_FILE_NAME);
+    if (!outputFile.is_open()) 
+    {
+        std::cout << "Failed to open output file!\n";
+        return false;
+    }
+    if (result == -1) 
+    {
+        outputFile << "No\n";
+    }
+    else 
+    {
+        outputFile << result << std::endl;
+    }
+    outputFile.close();
+    return true;
+}
+
+int main() 
+{
+    int n;
+    std::string inputStr;
+
+    if (!readInput(n, inputStr)) 
+    {
+        return 1;
+    }
+
+    int result = findMinShiftPosition(n, inputStr);
+
+    if (!writeOutput(result))
+    {
+        return 1;
+    }
+
+    return 0;
+}
---
+++ МЛИТА/Lab17.2/17.2/17.2.cpp
@@ -0,0 +1,145 @@
+﻿//17.2.Инверсии 2 (7)
+//Задан вектор инверсий перестановки X = (X1, X2, …, XN) чисел от до N(1≤ N ≤ 500000).
+// Требуется найти саму перестановку.
+//Пример
+//Ввод
+//5
+//0 0 1 3 2
+//Вывод
+//2 5 4 1 3
+//Бабина Мария ПС 21
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+const std::string INPUT_FILE_NAME = "input9.txt";
+const std::string OUTPUT_FILE_NAME = "output.txt";
+
+bool readInput(int& N, std::vector<int>& inversionsX)
+{
+    std::ifstream input(INPUT_FILE_NAME);
+    if (!input.is_open())
+    {
+        std::cout << "Failed to open input file!\n";
+        return false;
+    }
+
+    input >> N;
+    inversionsX.resize(N);
+    for (int i = 0; i < N; ++i)
+    {
+        input >> inversionsX[i];
+    }
+
+    input.close();
+    return true;
+}
+
+bool writeOutput(const std::vector<int>& permutation)
+{
+    std::ofstream output(OUTPUT_FILE_NAME);
+    if (!output.is_open())
+    {
+        std::cout << "Failed to open output file!\n";
+        return false;
+    }
+
+    for (int i = 0; i < permutation.size(); ++i)
+    {
+        output << permutation[i];
+        if (i != permutation.size() - 1)
+        {
+            output << " ";
+        }
+    }
+    output << "\n";
+
+    output.close();
+    return true;
+}
+
+void updateTree(std::vector<int>& tree, int index, int delta)
+{
+    while (index < static_cast<int>(tree.size()))
+    {
+        tree[index] += delta;
+        index += index & -index;
+    }
+}
+
+void initTree(std::vector<int>& tree, int N)
+{
+    for (int i = 1; i <= N; ++i)
+    {
+        updateTree(tree, i, 1);
+    }
+}
+
+int calcSum(const std::vector<int>& tree, int index) 
+{
+    int result = 0;
+    while (index > 0)
+    {
+        result += tree[index];
+        index -= index & -index;
+    }
+    return result;
+}
+
+int findElement(const std::vector<int>& tree, int k) 
+{
+    int left = 1, right = static_cast<int>(tree.size()) - 1;
+    int result = right;
+    while (left <= right) 
+    {
+        int middle = (left + right) / 2;
+        int sum = calcSum(tree, middle);
+        if (sum >= k) 
+        {
+            result = middle;
+            right = middle - 1;
+        }
+        else 
+        {
+            left = middle + 1;
+        }
+    }
+    return result;
+}
+
+std::vector<int> restorePermutation(int N, const std::vector<int>& inversionsX) 
+{
+    std::vector<int> tree(N + 1, 0);
+    initTree(tree, N);
+    
+    std::vector<int> P(N);
+    for (int i = N - 1; i >= 0; --i) 
+    {
+        int k = (i + 1) - inversionsX[i];
+        int num = findElement(tree, k);
+        P[i] = num;
+        updateTree(tree, num, -1);
+    }
+    return P;
+}
+
+int main() 
+{
+    int N;
+    std::vector<int> inversionsX;
+
+    if (!readInput(N, inversionsX))
+    {
+        return 1;
+    }
+
+    std::vector<int> permutation = restorePermutation(N, inversionsX);
+
+    if (!writeOutput(permutation)) 
+    {
+        return 1;
+    }
+
+    return 0;
+}
